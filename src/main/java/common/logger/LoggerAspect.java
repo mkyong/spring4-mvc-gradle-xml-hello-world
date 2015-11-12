48d4acb150fc79a131441cbbedd692d1bff77c2e
holy <holy1017@naver.com>
2015-11-12 16:41:12 +0900

    http://addio3305.tistory.com/89 따라하는중

--- src/main/java/common/logger/LoggerAspect.java
+++ src/main/java/common/logger/LoggerAspect.java
@@ -22,18 +22,20 @@ public class LoggerAspect {
 	 * @return
 	 * @throws Throwable
 	 */
-	@Around("execution(* first.*Controller.*(..)) or execution(* first.*Service.*(..)) or execution(* first.*DAO.*(..))")
+	@Around("execution(* first..*Controller.*(..)) or execution(* first..*Service.*(..)) or execution(* first..*DAO.*(..))")
 	public Object logPrint(ProceedingJoinPoint joinPoint) throws Throwable {
 		type = joinPoint.getSignature().getDeclaringTypeName();
 
-		if (type.indexOf("Controller") > -1) {
-			name = "Controller  \t:  ";
-		} else if (type.indexOf("Service") > -1) {
-			name = "ServiceImpl  \t:  ";
-		} else if (type.indexOf("DAO") > -1) {
-			name = "DAO  \t\t:  ";
-		}
+//		if (type.indexOf("Controller") > -1) {
+//			name = "Controller  \t: ";
+//		} else if (type.indexOf("Service") > -1) {
+//			name = "ServiceImpl  \t: ";
+//		} else if (type.indexOf("DAO") > -1) {
+//			name = "DAO  \t\t: ";
+//		}
 		log.debug(name + type + "." + joinPoint.getSignature().getName() + "()");
 		return joinPoint.proceed();
 	}
+	
+	
 }
